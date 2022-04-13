69a55e6b22d5a90f2ed365a6cd1f4ac837df48b8
Garik-S <garik.safronov@gmail.com>
2022-04-14 00:53:40 +0300

    hw5

--- src/main/java/ru/gb/ShopApp.java
+++ src/main/java/ru/gb/ShopApp.java
@@ -4,18 +4,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import ru.gb.config.HibernateConfig;
 import ru.gb.dao.ManufacturerDao;
 import ru.gb.dao.OldJdbcManufacturerDao;
+import ru.gb.dao.ProductDao;
 import ru.gb.entity.Manufacturer;
 
 public class ShopApp {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
-//        ManufacturerDao manufacturerDao = new OldJdbcManufacturerDao();
-        ManufacturerDao manufacturerDao = context.getBean(ManufacturerDao.class);
-//        System.out.println(manufacturerDao.findNameById(3L));
-        for (Manufacturer manufacturer : manufacturerDao.findAll()) {
-            System.out.println(manufacturer);
-        }
-//        System.out.println(manufacturerDao.findNameById(3L));
 
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
+
+        ProductDao productDao = context.getBean(ProductDao.class);
+//        for (Product product : productDao.findAll()) {
+//            System.out.println(product);
+//        }
+        System.out.println(productDao.findTitleById(5L));
     }
 }
--- src/main/java/ru/gb/entity/Product.java
+++ src/main/java/ru/gb/entity/Product.java
@@ -1,7 +1,10 @@
 package ru.gb.entity;
 
 import lombok.*;
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -11,9 +14,27 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @ToString
+@Entity
+@Table(name = "product")
+@NamedQueries({
+        @NamedQuery(name = "Product.findTitleById",
+                query = "select p.title from Product p where p.id = :id"),
+        @NamedQuery(name = "Product.findById",
+                query = "select p from Product p where p.id = :id")
+})
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "cost")
     private BigDecimal cost;
+
+    @Column(name = "date")
     private LocalDate date;
 }
